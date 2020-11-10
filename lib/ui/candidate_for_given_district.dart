d71e9bc252b288741a609f77f04aab2b21b984b5
Michael Kabali <kabalimichael03@gmail.com>
2020-11-10 12:27:42 +0300

    add new db and then add showing candidates of a user

--- lib/ui/candidate_for_given_district.dart
+++ lib/ui/candidate_for_given_district.dart
@@ -4,19 +4,20 @@ import 'package:working_with_sqlt/util/database_client.dart';
 
 class CandidateForAgivenDistrict extends StatefulWidget {
   @override
-  _CandidateForAgivenDistrictState createState() => _CandidateForAgivenDistrictState();
+  _CandidateForAgivenDistrictState createState() =>
+      _CandidateForAgivenDistrictState();
 }
 
-class _CandidateForAgivenDistrictState extends State<CandidateForAgivenDistrict> {
-  
+class _CandidateForAgivenDistrictState
+    extends State<CandidateForAgivenDistrict> {
   final DatabaseHelper databaseHelper = DatabaseHelper();
   TextEditingController _searchController = TextEditingController();
 
   List<Candidate> _searchResult = [];
   void _search() async {
     print('${_searchController.text}');
-    var result = await databaseHelper
-        .getLocationDetails(_searchController.text.toUpperCase().trim());
+    var result = await databaseHelper.getCandidateForGivenDistrict(
+        _searchController.text.toUpperCase().trim());
     setState(() {
       _searchResult = [];
       _searchResult = result;
@@ -36,7 +37,7 @@ class _CandidateForAgivenDistrictState extends State<CandidateForAgivenDistrict>
               child: TextFormField(
                 controller: _searchController,
                 decoration: InputDecoration(
-                    labelText: 'Enter electoral village',
+                    labelText: 'Enter district',
                     suffix: InkWell(
                       child: Text('Search'),
                       onTap: _search,
@@ -47,45 +48,51 @@ class _CandidateForAgivenDistrictState extends State<CandidateForAgivenDistrict>
                 padding: const EdgeInsets.all(8.0),
                 child: Container(
                   height: MediaQuery.of(context).size.height * 0.78,
-                  child: _searchResult.length != null ? ListView.builder(
-                    itemCount: _searchResult.length,
-                    itemBuilder: (context, index) => Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
+                  child: _searchResult.length != null
+                      ? ListView.builder(
+                          itemCount: _searchResult.length,
+                          itemBuilder: (context, index) => Column(
                             children: [
-                              Row(
-                                children: [
-                                  Text('${++index} District Name:'),
-                                  Text('${_searchResult[--index].districtName}'),
-                                ],
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Text('${++index} District Name:'),
+                                        Text(
+                                            '${_searchResult[--index].districtName}'),
+                                      ],
+                                    ),
+                                    Row(
+                                      children: [
+                                        Text('SurName:'),
+                                        Text('${_searchResult[index].surname}'),
+                                      ],
+                                    ),
+                                    Row(
+                                      children: [
+                                        Text('Other Name:'),
+                                        Text(
+                                            '${_searchResult[index].otherName}'),
+                                      ],
+                                    ),
+                                    Row(
+                                      children: [
+                                        Text('Political Party:'),
+                                        Expanded(
+                                          child: Text(
+                                              '${_searchResult[index].politicalParty}'),
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
                               ),
-                              Row(
-                                children: [
-                                  Text('SurName:'),
-                                  Text('${_searchResult[index].surname}'),
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  Text('Other Name:'),
-                                  Text('${_searchResult[index].otherName}'),
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  Text('Political Party:'),
-                                  Text(
-                                      '${_searchResult[index].politicalParty}'),
-                                ],
-                              )
                             ],
                           ),
-                        ),
-                      ],
-                    ),
-                  ): Container(),
+                        )
+                      : Container(),
                 )),
           ]),
         ));
